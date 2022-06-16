28a3d6e8593ce301364a115f16259f28719ad424
Kieu Minh Duc - NHSOS <nhsos37@shb.com.vn>
2022-06-16 10:09:26 +0700

    update issue 2

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Buffalo", Age = 25, Description = "hard - working" },
+            new Animal { Id = 2, Name = "Dragon", Age = 1000, Description = "fly" },
+            new Animal { Id = 3, Name = "Tiger", Age = 15, Description = "roaring" },
         };
     }
 }
